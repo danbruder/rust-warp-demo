a65d2c5ca4aa965396dff73bc1da6f52ab392efe
Dan Bruder <danb@structionsite.com>
2021-01-26 21:14:03 -0500

    appease the compiler

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-use parking_lot::RwLock;
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-use std::sync::{Arc};
-use uuid::Uuid;
-use warp::Filter;
-
-// We need to implement the "Clone" trait in order to
-// call the "cloned" method in the "get_dogs" route.
-#[derive(Clone, Debug, Deserialize, Serialize)]
-struct Dog {
-    id: String,
-    breed: String,
-    name: String,
-}
-
 #[derive(Debug, Deserialize, Serialize)]
 struct NewDog {
     breed: String,
@@ -37,26 +21,34 @@ async fn main() {
     };
     dog_map.write().insert(id, dog);
 
+    let dog_map_state = warp::any().map(move || dog_map.clone());
+
     // This is just for verifying that Warp is working.
     // Browse localhost:8000/hello/April/21
     let hello = warp::path!("hello" / String / u8)
         .map(|name, age| format!("Hello, {} year old named {}!", age, name));
 
     // Browse localhost:8000/dog/{id}
-    let get_dog = warp::path!("dog" / String).and_then(move |id: String| {
-        if let Some(dog) = dog_map.read().get(&id) {
-            Ok(warp::reply::json(&dog))
-        } else {
-            Err(warp::reject::not_found())
-        }
-    });
+    let get_dog = warp::path!("dog" / String)
+        .and(dog_map_state.clone())
+        .and_then(
+            |id: String, dog_map: Arc<RwLock<HashMap<_, _>>>| async move {
+                if let Some(dog) = dog_map.read().get(&id).as_ref() {
+                    Ok(warp::reply::json(&dog))
+                } else {
+                    Err(warp::reject::not_found())
+                }
+            },
+        );
 
     // Browse localhost:8000/dog
-    let get_dogs = warp::path!("dog").map(move || {
-        let dogs: Vec<Dog> = dog_map.read().values().cloned().collect();
-        warp::reply::json(&dogs)
-    });
-
+    let get_dogs =
+        warp::path!("dog")
+            .and(dog_map_state)
+            .map(move |dog_map: Arc<RwLock<HashMap<_, _>>>| {
+                let dogs: Vec<Dog> = dog_map.read().values().cloned().collect();
+                warp::reply::json(&dogs)
+            });
 
     //TODO: Learn how to get this to use TLS/HTTPS.
     let routes = hello.or(get_dogs).or(get_dog);
